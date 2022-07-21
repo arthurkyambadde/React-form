f07ed07b21bdca2ff2995f35d73d25025ba1f023
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-07-21 09:43:36 +0300

    Created form component file

---
+++ react-form/src/Components/FormComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Form = styled.div`
+  width: 100%;
+`;
+
+class FormComponent extends React.Component {
+  render() {
+    return <Form></Form>;
+  }
+}
+
+export default FormComponent;
