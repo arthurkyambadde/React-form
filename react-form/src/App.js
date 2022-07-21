d408a16695fc1ddf7cf361256d27ef3f612f26cf
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-07-21 13:00:26 +0300

    styled components to improve on UI

--- react-form/src/App.js
+++ react-form/src/App.js
@@ -4,10 +4,12 @@ import React from "react";
 
 const AppContainer = styled.div`
   width: 100vw;
+  height: 100vh;
   padding: 50px;
   display: flex;
   align-items: center;
   justify-content: center;
+  background-color: #4c91ad;
 `;
 
 class App extends React.Component {
